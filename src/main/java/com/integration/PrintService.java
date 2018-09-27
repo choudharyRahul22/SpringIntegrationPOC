c6145937ebd81a0fe1f5a0b24761c5ade1da5da4
choudharyRahul22 <choudharyRahul22@outlook.com>
2018-09-27 16:36:49 +0530

    pubsubchannel

--- src/main/java/com/integration/PrintService.java
+++ src/main/java/com/integration/PrintService.java
@@ -5,13 +5,6 @@ import org.springframework.messaging.Message;
 public class PrintService {
 
 	public void print(Message<String> message){
-		/*
-		 * If we throw exception than the messaging framework is smart enough 
-		 * it will route the message using load balancer again to other service.
-		 * 
-		 */
-		throw new RuntimeException("Custom error");
-		
-		/*System.out.println("Print service - " + message.getPayload());*/
+		System.out.println("Print service - " + message.getPayload());
 	}
 }
