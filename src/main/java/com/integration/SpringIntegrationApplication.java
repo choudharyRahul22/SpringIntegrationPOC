953e8c881a46fdbdb8b35fc6b174b1d38a65eac3
choudharyRahul22 <choudharyRahul22@outlook.com>
2018-09-27 11:08:14 +0530

    message

--- src/main/java/com/integration/SpringIntegrationApplication.java
+++ src/main/java/com/integration/SpringIntegrationApplication.java
@@ -1,45 +1,52 @@
 package com.integration;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.support.MessageBuilder;
 import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageHeaders;
-import org.springframework.messaging.support.GenericMessage;
+import org.springframework.messaging.MessageHandler;
+import org.springframework.messaging.MessagingException;
 
 @SpringBootApplication
 @Configuration
 @ImportResource("integration-context.xml")
-public class SpringIntegrationApplication implements ApplicationRunner{
+public class SpringIntegrationApplication implements ApplicationRunner {
+
+	@Autowired
+	/*
+	 * Direct Channel is a simple type of channel that connect 2 end point and
+	 * they are directly connected.
+	 */
+	private DirectChannel channel;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringIntegrationApplication.class, args);
 	}
-	
+
 	@Override
 	public void run(ApplicationArguments arg0) throws Exception {
-		
-		/*Map<String,Object> map = new HashMap<>();
-		map.put("Key", "Value");
-		MessageHeaders headers = new MessageHeaders(map);
-		
-		Message<String> message = new GenericMessage<String>("This is playload", headers);*/
-		
-		
-		/* Using Builder Pattern */
-		Message<String> message = MessageBuilder.withPayload("Using builder design pattern")
-												.setHeader("key", "value")
-												.setHeader("key2", "value2")
-												.build();
-		
-		PrintService service = new PrintService();
-		service.print(message);
+
+		/*
+		 * Subscription should be done before sending
+		 */
+		channel.subscribe(new MessageHandler() {
+			@Override
+			public void handleMessage(Message<?> message) throws MessagingException {
+				PrintService service = new PrintService();
+				service.print((Message<String>) message);
+			}
+		});
+
+		Message<String> message = MessageBuilder.withPayload("Using builder design pattern").setHeader("key", "value")
+				.setHeader("key2", "value2").build();
+
+		channel.send(message);
+
 	}
 }
